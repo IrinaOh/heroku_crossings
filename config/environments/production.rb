8f7e03b1d4c134e209aa40c29590a1d1c24d9f61
Irina O'Hara <irina.v.ohara@gmail.com>
2017-01-25 16:52:02 -0700

    heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost:3000',
+  domain:               'crossings.herokuapp.com',
   user_name:            ENV["MAILER_EMAIL"],
   password:             ENV["MAILER_PASSWORD"],
   authentication:       'plain',
